fd222f43b67d322f6d673dfae0d07b5729b3b4c1
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2022-02-07 00:27:01 -0400

    adicionando imagem

--- curso-react/src/App.js
+++ curso-react/src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'
 
 function App() {
+  const name = 'Italo'
+  const newName = name.toUpperCase()
+  const url = "https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+
+  function sum(a, b) {
+    return a+b;
+  }
+
   return (
     <div className="App">
       <h1>Ola React!</h1>
+      <p>Olá, {name}</p>
+      <p> -&gt; {newName}</p>
+      <p>Soma: { sum(2, 5)}</p>
+      <img src={url} alt="My image"/
+      >
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- fundamentos-react/src/components/App.jsx
+++ fundamentos-react/src/components/App.jsx
@@ -16,36 +16,36 @@ export default () => (
   <div id="app">
     <h1>Fundamentos React</h1>
     <div className="cards-flex">
-      <Card titulo="Lista de Produtos" color="#E2AEDD">
+      <Card titulo="Lista de Produtos" color="#E2AEDD" className="card">
         <ListaProdutos></ListaProdutos>
       </Card>
-      
-      <Card titulo="Repetição" color="">
+
+      <Card titulo="Repetição" className="card">
         <ListaAlunos></ListaAlunos>
       </Card>
 
-      <Card titulo="Familia">
+      <Card titulo="Familia" className="card">
         <Familia sobrenome="sobrenome teste" idade="23">
           <FamiliaMembro nome="Italo" />
           <FamiliaMembro nome="Ana" />
-          <FamiliaMembro nome="Pedro"/>
+          <FamiliaMembro nome="Pedro" />
           <FamiliaMembro />
         </Familia>
       </Card>
 
-      <Card titulo="Exemplo de card" color="#496DDB">
+      <Card titulo="Exemplo de card" color="#496DDB" className="card">
         <Random min={1} max={100} random />
       </Card>
 
-      <Card titulo="Fragmento" color="#533A7B">
+      <Card titulo="Fragmento" color="#533A7B" className="card">
         <Fragmento />
       </Card>
 
-      <Card titulo="Primeiro Componente" color="#EE8434">
+      <Card titulo="Primeiro Componente" color="#EE8434" className="card">
         <PrimeiroComponente />
       </Card>
 
-      <Card titulo="Com parametros" color="#5EC2B7">
+      <Card titulo="Com parametros" color="#5EC2B7" className="card">
         <ComParametro titulo="Situação do aluno" aluno="Italo" nota={8.3} />
       </Card>
     </div>
