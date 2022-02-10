b0ff535d25498a281aa2e4cb2990e595550014c6
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2022-02-10 02:19:09 -0400

    ATUALIZAÇÃO DE CONTEÚDO

--- curso-react/src/App.js
+++ curso-react/src/App.js
@@ -1,23 +1,39 @@
-
 import './App.css'
+import HelloWorld from './components/HelloWorld'
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa'
 
 function App() {
   const name = 'Italo'
   const newName = name.toUpperCase()
-  const url = "https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  const url =
+    'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
 
   function sum(a, b) {
-    return a+b;
+    return a + b
   }
+
+  const nome = 'Maria'
 
   return (
     <div className="App">
       <h1>Ola React!</h1>
       <p>Olá, {name}</p>
       <p> -&gt; {newName}</p>
-      <p>Soma: { sum(2, 5)}</p>
-      <img src={url} alt="My image"/
-      >
+      <p>Soma: {sum(2, 5)}</p>
+      <img src={url} alt="My image" />
+
+      <HelloWorld />
+      <SayMyName nome="Italo" />
+      <SayMyName nome="Bot" />
+      <SayMyName nome={nome} />
+
+      <Pessoa 
+        nome="Italo"
+        foto="https://via.placeholder.com/150"
+        idade="28"
+        profissao="Desenvolvedor"
+      />
     </div>
   )
 }
---
+++ curso-react/src/components/Frase.js
@@ -0,0 +1,9 @@
+function Frase(){
+  return(
+    <div>
+      <p>Esta é uma frase de um componente</p>
+    </div>
+  )
+}
+
+export default Frase;
