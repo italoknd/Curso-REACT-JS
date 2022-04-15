30a4298dc9d33be8e2dd39c7b4deffdde2d861f8
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2022-04-15 19:06:38 -0400

    desafio mega sena concluido

--- fundamentos-react/src/components/mega/Mega.jsx
+++ fundamentos-react/src/components/mega/Mega.jsx
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useState } from 'react'
 
 export default function Mega(props) {
-  return(
+  function gerarNumeros(qtde) {
+    const numeros = Array(qtde)
+      .fill(0)
+      .reduce(nums => {
+        const novoNumero = gerarNumeroNaoContido(1, 60, nums)
+        return [...nums, novoNumero]
+      }, [])
+      .sort((n1, n2) => n1 - n2)
+
+    return numeros
+  }
+
+  function gerarNumeroNaoContido(min, max, array) {
+    const random = parseInt(Math.random() * (max - min) + min)
+
+    return array.includes(random)
+      ? gerarNumeroNaoContido(min, max, array)
+      : random
+  }
+
+  const numerosIniciais = Array(props.qtde || 6).fill(0)
+  const [numeros, setNumeros] = useState(numerosIniciais)
+  const [qtde, setQtde] = useState(props.qtde || 6)
+
+  return (
     <div>
-      jsaubsakbdkaj
+      <h2>Mega</h2>
+      <h3>{numeros.join(' ')}</h3>
+      <div>
+        <label htmlFor="">Quantidade de Números: </label>
+        <br />
+        <input
+          type="number"
+          min="6"
+          max="14"
+          value={qtde}
+          onChange={_ => {
+            setQtde(+_.target.value)
+            setNumeros(gerarNumeros(+_.target.value))
+          }}
+        />
+      </div>
+      <button
+        onClick={_ => setNumeros(gerarNumeros(qtde))}
+        className="btn btn-primary"
+        style={{
+          marginTop: '.6em'
+        }}
+      >
+        Gerar Números
+      </button>
     </div>
   )
 }
