8deeeaac61be903a611f0bb6da8fd0719794bd82
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-03-06 11:40:59 -0400

    puttin all components into cards

--- react-fundamentals/src/App.tsx
+++ react-fundamentals/src/App.tsx
@@ -1,21 +1,27 @@
 import "./App.css";
+import Card from "./components/layout/Card";
 import Primeiro from "./components/basicos/Primeiro.jsx";
 import Props from "./components/basicos/Props.jsx";
 import NumeroAleatorio from "./components/basicos/NumeroAleatorio.jsx";
 
 function App() {
   return (
-    <>
-      <h1>Olá React!</h1>
-      <Primeiro />
-      <Props
-        titulo="Segundo Componente"
-        subtitulo="Criatividade Máxima!"
-        nota={6.78}
-        aluno="Italo Pedroza"
-      />
-      <NumeroAleatorio max_num={100} min_num={1} />
-    </>
+    <div className="containers">
+      <Card titulo="Desafio Número Aleatório">
+        <NumeroAleatorio max_num={100} min_num={1} />
+      </Card>
+      <Card titulo="Primeiro Contato">
+        <Primeiro />
+      </Card>
+      <Card titulo="Trabalhando Com Props">
+        <Props
+          titulo="Segundo Componente"
+          subtitulo="Criatividade Máxima!"
+          nota={6.78}
+          aluno="Italo Pedroza"
+        />
+      </Card>
+    </div>
   );
 }
 
