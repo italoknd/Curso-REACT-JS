5896ead805125cf977a29de5fdb05e98ff0de439
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-03-22 17:20:34 -0400

    new interface added

--- react-fundamentals/src/interfaces/basicos-interface/basico.ts
+++ react-fundamentals/src/interfaces/basicos-interface/basico.ts
@@ -1,7 +1,16 @@
+import { ReactElement } from "react";
+
 export interface ITitulos {
   titulo: string;
   subtitulo: string;
 
   nota: number;
   aluno: string;
+}
+
+export interface IDadosUsuario {
+  nome?: string;
+  sobrenome?: string;
+  idade?: string;
+  children?: ReactElement;
 }
