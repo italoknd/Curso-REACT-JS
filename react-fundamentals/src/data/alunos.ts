3e8a64e5832be992186532c342309d4d8c39c422
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-03-22 17:34:01 -0400

    creating a list of students

---
+++ react-fundamentals/src/data/alunos.ts
@@ -0,0 +1,11 @@
+import { IAluno } from "../interfaces/basicos-interface/basico";
+
+const alunos: IAluno = [
+  { id: 1, nome: "Ana", nota: 5.2 },
+  { id: 2, nome: "Italo", nota: 6.2 },
+  { id: 3, nome: "Carlos", nota: 7.2 },
+  { id: 4, nome: "Daniel", nota: 8.2 },
+  { id: 5, nome: "Hugo", nota: 9.2 }
+];
+
+export default alunos
