22f5f4969d1d18b046b291ee67031bab29d20ddd
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-03-22 09:56:21 -0400

    applying styles dynamically

--- react-fundamentals/src/components/layout/Card.tsx
+++ react-fundamentals/src/components/layout/Card.tsx
@@ -1,14 +1,18 @@
 import "./Card.css";
 
-const Card = (props) => {
+const Card = (props: any) => {
   return (
-    <div className="card">
+    <div
+      className="card"
+      style={{
+        backgroundColor: props.color,
+        border: `4px solid ${props.color} `
+      }}
+    >
       <div className="title">
         <h2>{props.titulo}</h2>
       </div>
-      <div className="content">
-        {props.children}
-      </div>
+      <div className="content">{props.children}</div>
     </div>
   );
 };
