9794d343d6cd6ce153f18f8b8f49f44e2d2ac4a8
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-03-22 18:03:23 -0400

    rendering the list of students with their grades

--- react-fundamentals/src/components/repeticao/ListaAlunos.tsx
+++ react-fundamentals/src/components/repeticao/ListaAlunos.tsx
@@ -1,11 +1,29 @@
 import React from "react";
+import "./ListaAlunos.css";
 
-const listaAlunos = (props) => {
+import dataAlunos from "../../data/alunos";
+import { IAluno } from "../../interfaces/basicos-interface/basico";
+
+const listaAlunos = props => {
+  const alunos: IAluno[] = dataAlunos;
   return (
     <>
-      <ul>
-        <li></li>
-      </ul>
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+          <th>Nota</th>
+        </tr>
+        <tr className="tdata">
+          {alunos.map((aluno: IAluno) => {
+            return (
+              <td>
+                {aluno.id} {aluno.nome} {aluno.nota}
+              </td>
+            );
+          })}
+        </tr>
+      </table>
     </>
   );
 };
