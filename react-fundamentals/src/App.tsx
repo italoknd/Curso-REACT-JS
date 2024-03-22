4e97077a8208a6e9847b2cf1e43572aa875a9bd0
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-03-22 17:34:52 -0400

    importing the new component on app tsx

--- react-fundamentals/src/App.tsx
+++ react-fundamentals/src/App.tsx
@@ -5,14 +5,18 @@ import Props from "./components/basicos/Props.jsx";
 import NumeroAleatorio from "./components/basicos/NumeroAleatorio.jsx";
 import Familia from "./components/basicos/Familia.jsx";
 import FamiliaMembro from "./components/basicos/FamiliaMembro.jsx";
+import ListaAlunos from "./components/repeticao/ListaAlunos";
 
 function App() {
   return (
     <div className="cards-container">
+      <Card titulo="Repetição" color="#b2f">
+        <ListaAlunos />
+      </Card>
       <Card titulo="Componentes Com Filhos" color="#e42">
         <Familia sobrenome="Pedroza">
           <FamiliaMembro nome="Italo" />
-          <FamiliaMembro nome="Daniel" />
+          <FamiliaMembro nome="Daniel" sobrenome="Sabino" />
           <FamiliaMembro nome="Hugo" />
         </Familia>
       </Card>
