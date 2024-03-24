f57dc616c316f0895e1fb1bc099ea8b336702b2a
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-03-24 13:07:59 -0400

    new component to render the list of products with monetrary value included

---
+++ react-fundamentals/src/components/repeticao/ListaProdutos.tsx
@@ -0,0 +1,34 @@
+import React from "react"
+import produtos from "../../data/produtos"
+import {formaMonetaryValue} from "../../utils/formatMonetaryValue"
+
+const listaProdutos = () => {
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Produto</th>
+                        <th scope="col">Valor</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        produtos.map(produto => {
+                            return (
+                                <tr key={produto.id}>
+                                    <td>{produto.id}</td>
+                                    <td>{produto.nome}</td>
+                                    <td>{formaMonetaryValue(Number(produto.valor))}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default listaProdutos
