a0e0abc3380b629f85818bdae0bf21776d4f7747
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-03-24 13:06:28 -0400

    creating a new file that contains a list of products

---
+++ react-fundamentals/src/data/produtos.ts
@@ -0,0 +1,9 @@
+const produtos = [
+    {id: 1, nome: "Farinha", valor: "18"},
+    {id: 2, nome: "Sabonete", valor: "2.50"},
+    {id: 3, nome: "Arroz", valor: "4"},
+    {id: 4, nome: "Água Sanitária", valor: "8.29"},
+    {id: 5, nome: "Macarrão", valor: "3.5"},
+]
+
+export default produtos
