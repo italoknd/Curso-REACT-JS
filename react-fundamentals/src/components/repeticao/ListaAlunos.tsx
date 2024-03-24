d7c754f37a8cda8ecb1de116527ae2cfbf5fa5a2
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-03-24 11:42:53 -0400

    html  and keyu corrections

--- react-fundamentals/src/components/repeticao/ListaAlunos.tsx
+++ react-fundamentals/src/components/repeticao/ListaAlunos.tsx
@@ -4,25 +4,29 @@ import "./ListaAlunos.css";
 import dataAlunos from "../../data/alunos";
 import { IAluno } from "../../interfaces/basicos-interface/basico";
 
-const listaAlunos = props => {
+const listaAlunos = (props) => {
   const alunos: IAluno[] = dataAlunos;
   return (
     <>
       <table>
-        <tr>
-          <th>ID</th>
-          <th>Nome</th>
-          <th>Nota</th>
-        </tr>
-        <tr className="tdata">
-          {alunos.map((aluno: IAluno) => {
-            return (
-              <td>
-                {aluno.id} {aluno.nome} {aluno.nota}
-              </td>
-            );
-          })}
-        </tr>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Nota</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="tdata">
+            {alunos.map((aluno: IAluno, index) => {
+              return (
+                <td key={index}>
+                  {aluno.id} {aluno.nome} {aluno.nota}
+                </td>
+              );
+            })}
+          </tr>
+        </tbody>
       </table>
     </>
   );
