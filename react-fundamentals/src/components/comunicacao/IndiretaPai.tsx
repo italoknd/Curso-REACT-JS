3915b65d34fa4325be501ade61b6e5150935d67a
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-03-30 12:20:00 -0400

    saving progress *

--- react-fundamentals/src/components/comunicacao/IndiretaPai.tsx
+++ react-fundamentals/src/components/comunicacao/IndiretaPai.tsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import IndiretaFilho from "./IndiretaFilho";
 import { IDadosUsuario } from "../../interfaces/basicos-interface/basico";
 
 const IndiretaPai = props => {
+  const [dadosUsuario, setDadosUsuario] = useState(null);
+
   const fornecerInformacoesDoUsuario = (dadosUsuario: IDadosUsuario) => {
-    const { nome, idade } = dadosUsuario;
-    console.log(`${nome} - ${idade}`);
+    setDadosUsuario(() => dadosUsuario);
   };
   return (
     <div>
       <h3>Pai</h3>
+      <p>Dados Recebidos do Componente Filho: {dadosUsuario}</p>
       <IndiretaFilho quandoClicar={fornecerInformacoesDoUsuario} />
     </div>
   );
