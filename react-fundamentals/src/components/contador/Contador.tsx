df5521b728a13e904f75b9addd548430b594e230
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-04-25 17:50:10 -0400

    creating a counter using component based in class

---
+++ react-fundamentals/src/components/contador/Contador.tsx
@@ -0,0 +1,68 @@
+import React, { Component, ReactEventHandler } from "react";
+import { IContador } from "./interfaceContador";
+
+export default class Contador extends Component<IContador> {
+  state = {
+    numero: this.props.numero,
+    passoIncremento: 0,
+    passoDecremento: 0,
+  };
+
+  inc = () => {
+    this.setState({
+      numero: this.state.numero + this.state.passoIncremento,
+    });
+  };
+
+  dec = () => {
+    this.setState({
+      numero: this.state.numero - this.state.passoDecremento,
+    });
+  };
+
+  inputValorInc: ReactEventHandler<HTMLInputElement> = (e) => {
+    if (e.target instanceof HTMLInputElement) {
+      this.setState({
+        passoIncremento: Number(e.target.value),
+      });
+    }
+  };
+
+  inputValorDec: ReactEventHandler<HTMLInputElement> = (e) => {
+    if (e.target instanceof HTMLInputElement) {
+      this.setState({
+        passoDecremento: Number(e.target.value),
+      });
+    }
+  };
+
+  render(): React.ReactNode {
+    return (
+      <div>
+        <p>{this.state.numero}</p>
+        <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+          <div className="input-container">
+            <label>valor de incremento:</label>
+            <input
+              type="number"
+              value={this.state.passoIncremento}
+              onChange={this.inputValorInc}
+            />
+          </div>
+          <div className="input-container">
+            <label>valor de decremento:</label>
+            <input
+              type="number"
+              value={this.state.passoDecremento}
+              onChange={this.inputValorDec}
+            />
+          </div>
+        </div>
+        <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+          <button onClick={this.inc}>Add number</button>
+          <button onClick={this.dec}>Remove number</button>
+        </div>
+      </div>
+    );
+  }
+}
