0ffa04ad0e8fc942b26d687de6ec380809bec5e5
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-05-01 11:28:50 -0400

    saving progress of the counter

--- react-fundamentals/src/components/contador/Contador.tsx
+++ react-fundamentals/src/components/contador/Contador.tsx
@@ -1,72 +1,38 @@
-import React, { Component, ReactEventHandler } from "react";
-import "./contador.css";
+import React, { Component } from "react";
+
+//interface
 import { IContador } from "./interfaceContador";
+
+//componentes
+import Display from "./Display";
+import FormContador from "./FormContador";
+import Controles from "./Controles";
 
 export default class Contador extends Component<IContador> {
   state = {
-    numero: this.props.numero,
-    passoIncremento: 0,
-    passoDecremento: 0,
+    numeroDisplay: 10,
+    passoIncremento: 20,
+    passoDecremento: 10,
   };
 
   inc = () => {
     this.setState({
-      numero: this.state.numero + this.state.passoIncremento,
+      numeroDisplay: this.state.numeroDisplay + this.state.passoIncremento,
     });
   };
 
   dec = () => {
     this.setState({
-      numero: this.state.numero - this.state.passoDecremento,
+      numeroDisplay: this.state.numeroDisplay - this.state.passoDecremento,
     });
-  };
-
-  inputValorInc: ReactEventHandler<HTMLInputElement> = (e) => {
-    if (e.target instanceof HTMLInputElement) {
-      this.setState({
-        passoIncremento: Number(e.target.value),
-      });
-    }
-  };
-
-  inputValorDec: ReactEventHandler<HTMLInputElement> = (e) => {
-    if (e.target instanceof HTMLInputElement) {
-      this.setState({
-        passoDecremento: Number(e.target.value),
-      });
-    }
   };
 
   render(): React.ReactNode {
     return (
       <div>
-        <p>{this.state.numero}</p>
-        <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
-          <div className="input-container">
-            <label>valor de incremento:</label>
-            <input
-              type="number"
-              value={this.state.passoIncremento}
-              onChange={this.inputValorInc}
-            />
-          </div>
-          <div className="input-container">
-            <label>valor de decremento:</label>
-            <input
-              type="number"
-              value={this.state.passoDecremento}
-              onChange={this.inputValorDec}
-            />
-          </div>
-        </div>
-        <div>
-          <button className="control-button" onClick={this.inc}>
-            +
-          </button>
-          <button className="control-button" onClick={this.dec}>
-            -
-          </button>
-        </div>
+        <Display numero={this.state.numeroDisplay} />
+        <FormContador />
+        <Controles decrementar={this.dec} incrementar={this.inc} />
       </div>
     );
   }
