3a6a1621cce6c0a7a5aa5ce1e444ecd50eb6e2ad
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-08-10 12:33:14 -0400

    salvando progresso *

--- react-fundamentals/src/components/calculadora/Calculadora.tsx
+++ react-fundamentals/src/components/calculadora/Calculadora.tsx
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
+import "./calculadora.css";
+import Button from "./components/Button";
 
-export default function Calculdadora() {
-  return <div>Calculadora React</div>;
+export default class Calculdadora extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Calculadora</h2>
+        <div className="calculadora">
+          <Button />
+        </div>
+      </div>
+    );
+  }
 }
---
+++ react-fundamentals/src/components/calculadora/components/Button.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./button.css";
+
+interface IButton {}
+
+export default function Button(props: IButton) {
+  return <button className="button">0</button>;
+}
