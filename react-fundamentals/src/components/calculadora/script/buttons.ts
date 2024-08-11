6e4eb0d99b4e8806fe6233fd8f0ecb593b89730b
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-08-11 13:58:11 -0400

    feat: criar script para manipular botoes

---
+++ react-fundamentals/src/components/calculadora/script/buttons.ts
@@ -0,0 +1,25 @@
+import { IButton } from "../interface/index";
+
+export const buttons: IButton[] = [
+  { label: "AC", labelClass: "" },
+  { label: "/", labelClass: "operation" },
+  { label: "7", labelClass: "" },
+  { label: "8", labelClass: "" },
+  { label: "9", labelClass: "" },
+
+  { label: "*", labelClass: "operation" },
+  { label: "4", labelClass: "" },
+  { label: "5", labelClass: "" },
+  { label: "6", labelClass: "" },
+
+  { label: "-", labelClass: "operation" },
+  { label: "1", labelClass: "" },
+  { label: "2", labelClass: "" },
+  { label: "3", labelClass: "" },
+
+  { label: "+", labelClass: "operation" },
+
+  { label: "0", labelClass: "double" },
+  { label: ".", labelClass: "" },
+  { label: "=", labelClass: "" },
+];
