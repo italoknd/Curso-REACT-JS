f1543c86729e356b7f3c2a8a5ef24bd7f74473aa
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-08-16 17:35:32 -0300

    feat: add interface para estado da calculadora

--- react-fundamentals/src/components/calculadora/interface/index.ts
+++ react-fundamentals/src/components/calculadora/interface/index.ts
@@ -1,5 +1,12 @@
-export  interface IButton {
-    label: string;
-    labelClass: string;
-    getValue?: Function;
-  }
+export interface IButton {
+  label: string;
+  labelClass: string;
+  getValue?: Function;
+}
+
+export interface ICalculatorState {
+  displayValue: string;
+  operationType: string;
+  value1: number;
+  value2: number;
+}
