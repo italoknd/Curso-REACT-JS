2e3d0c22f0ec4b62a05562595e378d90e1200988
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-08-17 12:45:25 -0300

    feat: criar estrutura de acesso as outras áreas do sistema

--- react-fundamentals/src/components/layout/Menu.tsx
+++ react-fundamentals/src/components/layout/Menu.tsx
@@ -2,7 +2,20 @@ import React from "react";
 import "./Menu.css";
 
 const Menu = (props: unknown) => {
-  return <aside>Menu</aside>;
+  return (
+    <aside className="menu">
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  );
 };
 
 export default Menu;
