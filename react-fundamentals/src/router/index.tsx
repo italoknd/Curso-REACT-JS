17c28a2149296167ffc589c9bb38c4ef6cf1aacb
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-08-17 12:46:08 -0300

    feat: criar nova rota para renderizar exemplo do menu

--- react-fundamentals/src/router/index.tsx
+++ react-fundamentals/src/router/index.tsx
@@ -1,15 +1,20 @@
 import Pratica from "../components/Praticas";
 import Calculdadora from "../components/calculadora/Calculator";
+import Parent from "../views/Parent";
 
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Pratica />
+    element: <Pratica />,
   },
   {
     path: "/calculadora",
-    element: <Calculdadora />
-  }
+    element: <Calculdadora />,
+  },
+  {
+    path: "/navigation-example",
+    element: <Parent />,
+  },
 ]);
