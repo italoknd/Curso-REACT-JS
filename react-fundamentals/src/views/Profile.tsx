1811c176be71c86dc06be6bfa6be9a4a60cc2cd8
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-08-18 16:46:37 -0400

    feat: adicionar uso do useParams

--- react-fundamentals/src/views/Profile.tsx
+++ react-fundamentals/src/views/Profile.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export default function Profiles() {
+  const params = useParams<{ profile: string }>();
+
   return (
     <div className="profiles">
-      <h2>Profile Section</h2>
+      <h2>Profile {params.profile}</h2>
       <button>
         <Link to={`/profiles`}>Go back to profiles</Link>
       </button>
