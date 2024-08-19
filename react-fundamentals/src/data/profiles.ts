0bff3ffa10ea6a5358a9b48a039aae40d08fb862
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-08-19 15:58:15 -0400

    feat: add lista de perfis

---
+++ react-fundamentals/src/data/profiles.ts
@@ -0,0 +1,53 @@
+import { IProfile } from "../interfaces/basicos-interface/profiles";
+
+export const profiles:IProfile[] = [
+  {
+    profileId: 1,
+    username: "Italo Pedroza",
+    age: 24,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo perspiciatis cum ut, rerum ab perferendis eligendi, soluta facilis enim, culpa cumque. Aperiam, repellat sed eveniet minus dolore incidunt ipsa molestiae!",
+  },
+  {
+    profileId: 2,
+    username: "Daniel Rodrigues",
+    age: 25,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo perspiciatis cum ut, rerum ab perferendis eligendi, soluta facilis enim, culpa cumque. Aperiam, repellat sed eveniet minus dolore incidunt ipsa molestiae!",
+  },
+  {
+    profileId: 3,
+    username: "Eduardo Oliveira",
+    age: 35,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo perspiciatis cum ut, rerum ab perferendis eligendi, soluta facilis enim, culpa cumque. Aperiam, repellat sed eveniet minus dolore incidunt ipsa molestiae!",
+  },
+  {
+    profileId: 4,
+    username: "Jairo Abreu",
+    age: 25,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo perspiciatis cum ut, rerum ab perferendis eligendi, soluta facilis enim, culpa cumque. Aperiam, repellat sed eveniet minus dolore incidunt ipsa molestiae!",
+  },
+  {
+    profileId: 5,
+    username: "Stevao Whinter",
+    age: 24,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo perspiciatis cum ut, rerum ab perferendis eligendi, soluta facilis enim, culpa cumque. Aperiam, repellat sed eveniet minus dolore incidunt ipsa molestiae!",
+  },
+  {
+    profileId: 6,
+    username: "João Hayden",
+    age: 24,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo perspiciatis cum ut, rerum ab perferendis eligendi, soluta facilis enim, culpa cumque. Aperiam, repellat sed eveniet minus dolore incidunt ipsa molestiae!",
+  },
+  {
+    profileId: 7,
+    username: "Fabricio Batista",
+    age: 36,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo perspiciatis cum ut, rerum ab perferendis eligendi, soluta facilis enim, culpa cumque. Aperiam, repellat sed eveniet minus dolore incidunt ipsa molestiae!",
+  },
+];
