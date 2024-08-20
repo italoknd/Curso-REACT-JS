599b088fc066b45ba336a792960e8f3f52bd2e37
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-08-20 18:46:24 -0400

    feat: renderizar prop do redux

--- react-fundamentals/src/views/Profile.tsx
+++ react-fundamentals/src/views/Profile.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { IProfile } from "../interfaces/basicos-interface/profiles";
 
 export default function Profiles() {
+  const profile = useSelector<{ profile: IProfile }>(
+    (state) => state.profile.profile
+  );
   const params = useParams<{ profile: string }>();
 
   return (
     <div className="profiles">
+      {profile.username}
       <h2>Profile {params.profile}</h2>
       <button>
         <Link to={`/profiles`}>Go back to profiles</Link>
