0b154326f6b661ca64ec76322511c4da6581ed55
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-08-22 17:51:21 -0400

    feat: renderizar card de perfil e corrigir tipagem do useSelector

--- react-fundamentals/src/views/Profile.tsx
+++ react-fundamentals/src/views/Profile.tsx
@@ -2,18 +2,25 @@ import React from "react";
 import { Link, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { IProfile } from "../interfaces/basicos-interface/profiles";
+import "./profile.css";
 
 export default function Profiles() {
-  const profile = useSelector<{ profile: IProfile }>(
-    (state) => state.profile.profile
-  );
-  const params = useParams<{ profile: string }>();
+  const { profile } = useSelector<{ profile: IProfile }>(
+    ({ profile }) => profile
+  ) as {
+    profile: IProfile;
+  };
+  // const params = useParams<{ profile: string }>();
 
   return (
-    <div className="profiles">
-      {profile.username}
-      <h2>Profile {params.profile}</h2>
-      <button>
+    // <h2>Profile {params.profile}</h2>
+    <div className="main-profile-container">
+      <div className="info">
+        <h2>Name: {profile.username}</h2>
+        <h2>Age: {profile.age}</h2>
+      </div>
+      <div className="desc">{profile.description}</div>
+      <button className="button">
         <Link to={`/profiles`}>Go back to profiles</Link>
       </button>
     </div>
