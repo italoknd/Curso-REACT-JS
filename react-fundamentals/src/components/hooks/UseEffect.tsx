797ecf35f5517cad97a2c11d4cfc1d5a658c89e8
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-09-02 18:40:13 -0300

    feat: praticando useEffect

--- react-fundamentals/src/components/hooks/UseEffect.tsx
+++ react-fundamentals/src/components/hooks/UseEffect.tsx
@@ -1,10 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SectionTitle from "../layout/SectionTitle";
 import "./hooks.css";
 
 export default function UseState() {
-  const [count, setCount] = useState<number>(0);
-  const [name, setName]=useState<string>('Italo')
+  //exemplo 1
+  const [count, setCount] = useState<number>(1);
+  const [fatorial, setFatorial] = useState<number>(1);
+
+  const calcFatorial = (number: number): number => {
+    if (number < 0 || number === 0) return 1;
+
+    return calcFatorial(number - 1) * number;
+  };
+
+  useEffect(() => {
+    setFatorial(calcFatorial(count));
+  }, [count]);
+
+  //exemplo 2
+  const [numero, setNumero] = useState<number>(1);
+  const [duplicado, setDuplicado] = useState<number>(1);
+
+  useEffect(() => {
+    setDuplicado(numero * 2);
+  }, [numero]);
 
   return (
     <div className="main-container-hooks">
@@ -15,17 +34,35 @@ export default function UseState() {
         <p>Permite executar efeitos colaterais em componentes funcionais.</p>
       </div>
 
-      <SectionTitle title="Exercício 01" />
+      <br />
+      <SectionTitle title="Exercício 01 - Fatorial (Usando useEffect)" />
+      <br />
       <div className="exercise-box">
-        <p style={{color: 'white'}}>{count}</p>
-        <button className="button" style={{color: 'white'}} onClick={() => setCount(count + 1)}>+</button>
-        <button className="button" style={{color: 'white'}} onClick={() => setCount(count - 1)}>-</button>
-
-        <div className="text-container">
-          <input type="text" onChange={(e)=> setName(e.target.value)} value={name}/>
-          <textarea value={name}/>
-        </div>
+        <h2 style={{ color: "white" }}>
+          Fatorial:{" "}
+          <span style={{ color: "#bb3333" }}>
+            {count > 0 ? fatorial : "Não existe"}
+          </span>
+        </h2>
+        <br />
+        <input
+          type="number"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
       </div>
+
+      <hr />
+      <br />
+      <SectionTitle title="Exercício 02 - Duplicação numérica" />
+      <input
+        type="number"
+        value={numero}
+        onChange={(e) => {
+          setNumero(Number(e.target.value));
+        }}
+      />
+      <h3 style={{ color: "white" }}>Número Duplicado: {duplicado}</h3>
     </div>
   );
 }
