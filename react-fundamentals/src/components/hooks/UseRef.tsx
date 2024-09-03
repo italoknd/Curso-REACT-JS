3693ff4d3bdf7f2b9d57e90be2bcab1ad31c325f
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-09-03 00:08:03 -0400

    feat: usando useRef

--- react-fundamentals/src/components/hooks/UseRef.tsx
+++ react-fundamentals/src/components/hooks/UseRef.tsx
@@ -1,8 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import SectionTitle from "../layout/SectionTitle";
 import "./hooks.css";
 
-export default function UseState() {
+export default function UseRef() {
+  const [value1, setValue1] = useState<string>("");
+  const count = useRef(0);
+
+  useEffect(() => {
+    count.current = count.current + 1;
+  }, [value1]);
+
   return (
     <div className="main-container-hooks">
       <div className="info">
@@ -15,7 +22,22 @@ export default function UseState() {
       </div>
 
       <br />
-      <SectionTitle title="Exercício 01 - ???? (Usando useRef)" />
+      <SectionTitle title="Exercício 01" />
+
+      <div className="flex justify-center">
+        <span className="color-white">Valor: </span>
+        <span className="color-white">{value1}</span>
+        <span className="color-white">
+          [<span style={{ color: "red" }}>{count.current}</span>]
+        </span>
+
+        <input
+          type="text"
+          value={value1}
+          onChange={(e) => setValue1(e.target.value)}
+        />
+      </div>
+      <SectionTitle title="Exercício 02" />
     </div>
   );
 }
