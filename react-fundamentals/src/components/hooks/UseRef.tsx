61b4e6da080efd1de9aad92f63830947360ea4cf
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2024-09-03 17:57:35 -0400

    feat: concluir desafio de merge dos valores dos inputs

--- react-fundamentals/src/components/hooks/UseRef.tsx
+++ react-fundamentals/src/components/hooks/UseRef.tsx
@@ -3,12 +3,25 @@ import SectionTitle from "../layout/SectionTitle";
 import "./hooks.css";
 
 export default function UseRef() {
-  const [value1, setValue1] = useState<string>("");
+  const [value1, setValue1] = useState<string[]>([]);
+  const [value2, setValue2] = useState<string[]>([]);
+  const [mergedValue, setMergedValue] = useState<string>("");
+
+  const inputRef = useRef("");
+  const input2Ref = useRef("");
   const count = useRef(0);
 
   useEffect(() => {
-    count.current = count.current + 1;
+    input2Ref.current.focus();
+    const lastPos = value1[value1.length - 1] ?? "";
+    setMergedValue(mergedValue + lastPos);
   }, [value1]);
+
+  useEffect(() => {
+    inputRef.current.focus();
+    const lastPos = value2[value2.length - 1] ?? "";
+    setMergedValue(mergedValue + lastPos);
+  }, [value2]);
 
   return (
     <div className="main-container-hooks">
@@ -26,18 +39,28 @@ export default function UseRef() {
 
       <div className="flex justify-center">
         <span className="color-white">Valor: </span>
-        <span className="color-white">{value1}</span>
+        <span className="color-white">{mergedValue}</span>
         <span className="color-white">
           [<span style={{ color: "red" }}>{count.current}</span>]
         </span>
-
+        <br />
+        <br />
+      </div>
+      <input
+        type="text"
+        value={value1}
+        ref={inputRef}
+        onChange={(e) => setValue1(e.target.value)}
+      />
+      <SectionTitle title="Exercício 02" />
+      <div className="flex justify-center">
         <input
           type="text"
-          value={value1}
-          onChange={(e) => setValue1(e.target.value)}
+          value={value2}
+          ref={input2Ref}
+          onChange={(e) => setValue2(e.target.value)}
         />
       </div>
-      <SectionTitle title="Exercício 02" />
     </div>
   );
 }
